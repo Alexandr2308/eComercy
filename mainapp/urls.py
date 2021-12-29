9c86e929e6d0461118b96cb256d89e807f061050
aleksandrgorlac <gorlach2308@gmail.com>
2021-12-29 18:50:42 +0200

    shop

---
+++ mainapp/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from .views import *
+
+
+urlpatterns = [
+    path('', index_view, name='home'),
+    path('shop/', shop_view, name='shop'),
+    path('single-product-details/', single_product_details_view, name='single-product-details'),
+    path('checkout/', checkout_view, name='checkout'),
+    path('about-us/', regular_page_view, name='about-us'),
+    path('contact/', contact_view, name='contact'),
+]
+
+
